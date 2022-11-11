481ea2e6676841bd47d8a97644a15fc5b854bf9e
nhan <chinhanhoan@gmail.com>
2022-11-11 23:30:29 +0700

    add requests

--- resources/views/DashboardTemplate/Employer/editEmployer.blade.php
+++ resources/views/DashboardTemplate/Employer/editEmployer.blade.php
@@ -11,6 +11,15 @@
                 </div>
             </div><!-- /.container-fluid -->
         </section>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <section class="content">
             <form action="{{ route('AdminEmloyer.update', $employer->id) }}" method="post" enctype="multipart/form-data">
                 @csrf
