2c9203411af95cb4619f170d5d7249255cfcacf1
nhan <chinhanhoan@gmail.com>
2022-11-11 14:12:22 +0700

    edit index

--- resources/views/DashboardTemplate/Jobposting/editJobposting.blade.php
+++ resources/views/DashboardTemplate/Jobposting/editJobposting.blade.php
@@ -8,19 +8,11 @@
                     <div class="col-sm-6">
                         <h1>Edit Job Posting</h1>
                     </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item"><a href="#">Layout</a></li>
-                            <li class="breadcrumb-item active">Fixed Layout</li>
-                        </ol>
-                    </div>
                 </div>
             </div><!-- /.container-fluid -->
         </section>
         <section class="content">
-            <form action="{{ route('AdminJobposting.update', $jobposting->id) }}" method="post"
-                enctype="multipart/form-data">
+            <form action="{{ route('AdminJobposting.update', $jobposting->id) }}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
                 <div class="row">
