50ea34714a5e5a967587d70a5dc7778e657c2214
tuanle1293 <93644812+tuanle1293@users.noreply.github.com>
2022-11-27 11:15:57 +0700

    Update RUEmployerController.php

--- app/Http/Controllers/RUEmployerController.php
+++ app/Http/Controllers/RUEmployerController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Employer;
+use App\Models\Customer;
 use App\Models\Recruitment;
-use App\Http\Requests\Employer\UpdateRequest;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\File;
+use Carbon\Carbon;
+use App\Models\Job_posting;
+use App\Http\Requests\Employer\UpdateRequest;
+use App\Http\Requests\Employer\ChangePasswordRequest;
 
 class RUEmployerController extends Controller
 {
@@ -49,8 +58,13 @@ class RUEmployerController extends Controller
      */
     public function show($id)
     {
-        $showEmploy = Employer::find($id);
-        return view('DashboardTemplate.employer.detail_employer',compact('showEmploy'));
+        $id = Auth::user()->employer->id;
+        $showEmploy = DB::table('employers')->where('id', $id)->first();
+        if ($showEmploy == null) {
+            return view('error.404');
+        } else {
+            return view('DashboardTemplate.employer.detail_employer', compact('showEmploy'));
+        }
     }
 
     /**
@@ -61,8 +75,13 @@ class RUEmployerController extends Controller
      */
     public function edit($id)
     {
-        $showEmploy = Employer::find($id);
-        return view('DashboardTemplate.employer.edit_employer',compact('showEmploy'));
+        $id = Auth::user()->employer->id;
+        $showEmploy = DB::table('employers')->where('id', $id)->first();
+        if ($showEmploy == null) {
+            return view('error.404');
+        } else {
+            return view('DashboardTemplate.employer.edit_employer', compact('showEmploy'));
+        }
     }
 
     /**
@@ -75,15 +94,18 @@ class RUEmployerController extends Controller
     public function update(UpdateRequest $request, $id)
     {
         $Employer = Employer::find($id);
-        if($request->hasFile('image_upload'))
-        {
+        if ($request->hasFile('image_upload')) {
             $file = $request->file('image_upload');
             $file_name = $file->getClientOriginalName();
-            $file->move('img',$file_name);
+            $file->move('img', $file_name);
+            $imagePath = public_path('img/' . $Employer->image);
+            if (File::exists($imagePath)) {
+                unlink($imagePath);
+            }
             $Employer->image = $file_name;
         }
         $Employer->update($request->all());
-        return redirect()->back()->with('notify','Update Susscessfully');
+        return redirect()->back()->with('notify', 'Update Susscessfully');
     }
 
     /**
@@ -94,8 +116,68 @@ class RUEmployerController extends Controller
      */
     public function destroy($id)
     {
-        $re = DB::table('recruitments')->select('id')->where('customer_id','=',$id)->get();
-        Recruitment::destroy($re[0]->id);
-        return redirect()->route('CRUDJobPosting.index');
+        $check = DB::table('recruitments')->where('id', $id)->first();
+        if ($check == null) {
+            return view('error.404');
+        } else {
+            $re = DB::table('recruitments')->select('id')->where('customer_id', '=', $id)->get();
+            Recruitment::destroy($re[0]->id);
+            return redirect()->route('CRUDJobByEmployer.show', Auth::user()->employer->id)->with('notify', 'Delete Susscessfully');
+        }
+    }
+
+    public function detail_recruitment($id)
+    {
+        $customer = DB::table('customers')->where('id', $id)->first();
+        if ($customer == null) {
+            return view('error.404');
+        } else {
+            $re = DB::table('recruitments')->select('id')->where('customer_id', '=', $id)->get();
+            $update_status = Recruitment::find($re[0]->id);
+            $update_status->update([
+                'status' => '1',
+            ]);
+            return view('DashboardTemplate.Job_postings.detail', compact('customer'));
+        }
+    }
+    public function recruit(Customer $customer)
+    {
+        $id = Customer::find($customer->id);
+        $job = $id->job_postings->take(1);
+        $tomorrow = Carbon::tomorrow();
+        Mail::send('DashboardTemplate.emails.send', compact('customer', 'job', 'tomorrow'), function ($email) use ($customer) {
+            $email->subject('Thông báo tuyển dụng');
+            $email->to($customer->email);
+        });
+        return redirect()->back()->with('notify', 'Send mail susscrssfully !');
+    }
+
+    public function changepass(ChangePasswordRequest $request, $id)
+    {
+        $id = DB::table('users')->where('id', $id)->first();
+        if ($id == null) {
+            return view('error.404');
+        } else {
+            if (!Hash::check($request->old_pass, auth()->user()->password)) {
+                return redirect()->back()->with("error", "Đổi mật khẩu thất bại!");
+            } else {
+                $change = User::find($id);
+                $change->update([
+                    'password' => $request->new_pass,
+                ]);
+                Auth::logout();
+                return redirect()->route('login')->with("status", "Đổi mật khẩu thành công, Vui lòng đăng nhập lại!");
+            }
+        }
+    }
+    public function showlayout($id)
+    {
+        $id = Auth::user()->id;
+        $showEmployByID = DB::table('users')->where('id', $id)->first();
+        if ($showEmployByID == null) {
+            return view('error.404');
+        } else {
+            return view('DashboardTemplate.employer.showlayout', compact('showEmployByID'));
+        }
     }
 }
