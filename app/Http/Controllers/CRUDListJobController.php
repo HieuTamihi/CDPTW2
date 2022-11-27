bc77d1a5cf99d8fcff7a8837f329d8563aa0b406
tuanle1293 <93644812+tuanle1293@users.noreply.github.com>
2022-11-27 11:14:20 +0700

    Update CRUDListJobController.php

--- app/Http/Controllers/CRUDListJobController.php
+++ app/Http/Controllers/CRUDListJobController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\Job_posting\StoreRequest;
 use App\Http\Requests\Job_posting\UpdateRequest;
+use App\Models\Recruitment;
 
 class CRUDListJobController extends Controller
 {
@@ -22,7 +23,7 @@ class CRUDListJobController extends Controller
         $id = Auth::user()->employer->id;
         $getPostByID = Employer::findOrFail($id);
         $result = $getPostByID->jobs;
-        return view('DashboardTemplate.Job_postings.list_post_by_id',compact('result'));
+        return view('DashboardTemplate.Job_postings.list_post_by_id', compact('result'));
     }
 
     /**
@@ -45,15 +46,15 @@ class CRUDListJobController extends Controller
     {
         Job_posting::create([
             'employer_id' => Auth::user()->employer->id,
-            'title'=>$request->title,
-            'experience' => $request->experience,
-            'type' => $request->type,
-            'skill' => $request->skill,
-            'required' => $request->required,
-            'salary' => $request->salary,  
+            'title' => htmlspecialchars($request->title),
+            'experience' => htmlspecialchars($request->experience),
+            'type' => htmlspecialchars($request->type),
+            'skill' => htmlspecialchars($request->skill),
+            'required' => htmlspecialchars($request->required),
+            'salary' => htmlspecialchars($request->salary),
             'token' => md5(Auth::user()->id),
         ]);
-        return redirect()->route('CRUDJobByEmployer.index')->with('notify','Add news is successfully');
+        return redirect()->route('CRUDJobByEmployer.index')->with('notify', 'Add news is successfully');
     }
 
     /**
@@ -64,9 +65,17 @@ class CRUDListJobController extends Controller
      */
     public function show($id)
     {
-        $show = Job_posting::find($id);
-        $getstatus = DB::table('recruitments')->where('jobposting_id','=',$id);
-        return view('DashboardTemplate.Job_postings.detail_post',compact('show'));
+        $show = DB::table('Job_postings')->where('id',$id)->where(function ($que){
+            $que->where('employer_id',Auth::user()->employer->id);
+        })->first() ;
+        if ($show == null) {
+            return view('error.404');
+        } else {
+            $show = Job_posting::findOrFail($show->id);
+            $list_recruitmet = $show->customers()->paginate(5);
+            $getstatus = DB::table('recruitments')->where('jobposting_id', '=', $id);
+            return view('DashboardTemplate.Job_postings.detail_post', compact('show','list_recruitmet'));
+        }
     }
 
     /**
@@ -77,8 +86,14 @@ class CRUDListJobController extends Controller
      */
     public function edit($id)
     {
-        $show = Job_posting::find($id);
-        return view('DashboardTemplate.Job_postings.edit_post',compact('show'));
+        $show = DB::table('Job_postings')->where('id',$id)->where(function ($que){
+            $que->where('employer_id',Auth::user()->employer->id);
+        })->first() ;
+        if ($show == null) {
+            return view('error.404');
+        }else {
+            return view('DashboardTemplate.Job_postings.edit_post', compact('show'));
+        }
     }
 
     /**
@@ -92,7 +107,7 @@ class CRUDListJobController extends Controller
     {
         $job = Job_posting::find($id);
         $job->update($request->all());
-        return redirect()->route('CRUDJobByEmployer.index')->with('notify','Update Susscessfully');
+        return redirect()->route('CRUDJobByEmployer.index')->with('notify', 'Update Susscessfully');
     }
 
     /**
@@ -105,10 +120,11 @@ class CRUDListJobController extends Controller
     {
         $token = md5(Auth::user()->employer->id);
         DB::table('Job_postings')
-        ->where([
-            'id' => $id,
-            'token' => $token])
-        ->delete();
-        return redirect()->route('CRUDJobByEmployer.index')->with('notify','Delete Susscessfully');
+            ->where([
+                'id' => $id,
+                'token' => $token
+            ])
+            ->delete();
+        return redirect()->route('CRUDJobByEmployer.index')->with('notify', 'Delete Susscessfully');
     }
 }
