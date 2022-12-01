660f97aeb4c604af4ae763e0dde159487f0a53ad
tuanle1293 <93644812+tuanle1293@users.noreply.github.com>
2022-12-01 21:35:24 +0700

    Update RegisterRequest.php

--- app/Http/Requests/User/RegisterRequest.php
+++ app/Http/Requests/User/RegisterRequest.php
@@ -27,7 +27,7 @@ class RegisterRequest extends FormRequest
             'name_company' => 'required',
             'address' => 'required',
             'email' => 'required|email',
-            'phone' => 'required|numeric|digits:10',
+              'phone' => 'required|numeric|digits:10|unique:users',
             'password' => 'required|confirmed|min:6',
         ];
     }
@@ -41,6 +41,7 @@ class RegisterRequest extends FormRequest
             'phone.required' => 'Please enter :attribute',
             'phone.numeric' => ':attribute is number',
             'phone.digits' => ':attribute more than 10 character',
+            'phone.unique' => ':attribute is exist',
             'password.required' => 'Please enter :attribute',
             'password.confirmed' => ':attribute is correct',
             'password.min' => ':attribute more than 6 character',
